adee6ded4e834e884241465d89431b6bc6547c97
ogino <ogino@ykw.elec.keio.ac.jp>
2019-01-04 21:52:53 +0900

    move the definition of `AreaSelector` to a separate file

---
+++ area_selector.py
@@ -0,0 +1,44 @@
+from PyQt5.QtWidgets import QWidget, QRubberBand
+from PyQt5.QtGui import QColor, QPainter
+from PyQt5.QtCore import Qt, QRect, pyqtSignal
+
+class AreaSelector(QWidget):
+    areaSelected = pyqtSignal(['QRect'])
+
+    def __init__(self):
+        super().__init__()
+        self.rubberBand = QRubberBand(QRubberBand.Rectangle, parent = self)
+        self.setWindowFlag(Qt.FramelessWindowHint)
+        self.setAttribute(Qt.WA_TranslucentBackground, True)
+        self.setWindowState(Qt.WindowFullScreen)
+        self.setCursor(Qt.CrossCursor)
+
+    def exec(self, callback):
+        self.areaSelected.connect(callback)
+        self.show()
+
+    def paintEvent(self, event):
+        p = QPainter(self)
+        p.fillRect(self.rect(), QColor(0, 0, 0, 1))
+
+    def set_geometry(self, top_left, bottom_right):
+        rect = QRect(top_left, bottom_right)
+        self.rubberBand.setGeometry(rect)
+
+    def set_bottom_right(self, bottom_right):
+        top_left = self.origin
+        self.set_geometry(top_left, bottom_right)
+
+    def mousePressEvent(self, event):
+        self.origin = event.pos()
+        self.set_geometry(self.origin, self.origin)
+        self.rubberBand.show()
+
+    def mouseMoveEvent(self, event):
+        pos = event.pos()
+        self.set_bottom_right(pos)
+
+    def mouseReleaseEvent(self, event):
+        rect = self.rubberBand.geometry()
+        self.close()
+        self.areaSelected.emit(rect)
--- main.py
+++ main.py
@@ -1,49 +1,8 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QRubberBand
-from PyQt5.QtGui import QPixmap, QGuiApplication, QPalette, QColor, QPainter
-from PyQt5.QtCore import Qt, QRect, pyqtSignal
-
-class AreaSelector(QWidget):
-    areaSelected = pyqtSignal(['QRect'])
-
-    def __init__(self):
-        super().__init__()
-        self.rubberBand = QRubberBand(QRubberBand.Rectangle, parent = self)
-        self.setWindowFlag(Qt.FramelessWindowHint)
-        self.setAttribute(Qt.WA_TranslucentBackground, True)
-        self.setWindowState(Qt.WindowFullScreen)
-        self.setCursor(Qt.CrossCursor)
-
-    def exec(self, callback):
-        self.areaSelected.connect(callback)
-        self.show()
-
-    def paintEvent(self, event):
-        p = QPainter(self)
-        p.fillRect(self.rect(), QColor(0, 0, 0, 1))
-
-    def set_geometry(self, top_left, bottom_right):
-        rect = QRect(top_left, bottom_right)
-        self.rubberBand.setGeometry(rect)
-
-    def set_bottom_right(self, bottom_right):
-        top_left = self.origin
-        self.set_geometry(top_left, bottom_right)
-
-    def mousePressEvent(self, event):
-        self.origin = event.pos()
-        self.set_geometry(self.origin, self.origin)
-        self.rubberBand.show()
-
-    def mouseMoveEvent(self, event):
-        pos = event.pos()
-        self.set_bottom_right(pos)
-
-    def mouseReleaseEvent(self, event):
-        rect = self.rubberBand.geometry()
-        self.close()
-        self.signal.emit(rect)
-
+from PyQt5.QtWidgets import QApplication, QLabel
+from PyQt5.QtGui import QGuiApplication
+from PyQt5.QtCore import Qt
+from area_selector import AreaSelector
 
 class Qapture(QLabel):
     def __init__(self):
